f896c0df5308ecaec29b1c71718a7f4e9fdfdcce
Hiroshi Muto <mhi6o461o@gmail.com>
2023-10-12 22:16:30 +0900

    feat: create toggle function

--- js/todoItemView.js
+++ js/todoItemView.js
@@ -4,9 +4,18 @@ var TodoItemView = Backbone.View.extend({
     if ((!options && options.model)) {
       throw new Error("model is not specified.");
     }
+    this.model.on("change", this.render, this);
+  },
+  events: {
+    "click #toggle": "onClickToggle"
+  },
+  onClickToggle: function () {
+    this.model.toggle();
   },
   render: function () {
-    this.$el.html(this.model.escape("description"));
+    this.$el.toggleClass("completed", this.model.get("isCompleted"));
+    var checked = this.model.get("isCompleted") ? "checked" : "";
+    this.$el.html("<input id='toggle' type='checkbox'" + checked + "></input>" + this.model.escape("description"));
     return this;
   }
 });
