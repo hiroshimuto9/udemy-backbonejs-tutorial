7b9d35547a1737ed77e1cf098f0d4563e7fa7361
Hiroshi Muto <mhi6o461o@gmail.com>
2023-10-12 21:42:07 +0900

    feat: create add todoItem function

--- js/todoItemsView.js
+++ js/todoItemsView.js
@@ -5,9 +5,34 @@ var TodoItemsView = Backbone.View.extend({
     if (!(options && options.model)) {
       throw new Error("model is not specified.");
     }
+    this.model.on("add", this.onAddTodoItem, this);
+  },
+  onAddTodoItem: function (todoItem) {
+    var view = new TodoItemView({ model: todoItem });
+    this.$el.append(view.render().$el);
+  },
+  events: {
+    "click #add": "onClickAdd",
+    "keypress #newTodoItem": "onKeyPress"
+  },
+  onClickAdd: function () {
+    var $textBox = this.$("#newTodoItem");
+    if ($textBox.val()) {
+      var todoItem = new TodoItem({ description: $textBox.val() });
+      this.model.add(todoItem);
+      $textBox.val("");
+    }
+  },
+  onKeyPress: function (e) {
+    if (e.keyCode == 13) {
+      this.onClickAdd();
+    }
   },
   render: function () {
     var self = this;
+
+    this.$el.append("<input type='text' autofocus id='newTodoItem'></input>");
+    this.$el.append("<button id='add'>Add</button>");
     this.model.each(function (todoItem) {
       var view = new TodoItemView({ model: todoItem });
       self.$el.append(view.render().$el);
